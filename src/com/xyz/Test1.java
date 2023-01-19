9de8d14ba7df20997b61a493022bbd1b1d78dbab
sakshiwaghmare <sakshiwaghmare85@gmail.com>
2023-01-19 11:56:07 +0530

    commited

--- src/com/xyz/Test1.java
+++ src/com/xyz/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		String hungry = "yes";
 		if (hungry == "yes")
-			System.out.println("eat something");
+			System.out.println("eat something new");
 		else {
 			System.out.println("Do work");
 		}
